5319e3a5e35f4f10fdadfe9d22d0acc56c9aa2d7
Jesus Figueroa <jbeltran@colef.mx>
2023-08-07 16:04:58 -0700

    erer

--- Program.cs
+++ Program.cs
@@ -95,7 +95,6 @@ app.UseSwagger();
 app.UseSwaggerUI(options =>
 {
     options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
 });
 //app.UseSwaggerUI();
 app.Run();
